648359808c9836474952884bf813d00975921562
L3OL1N <z3650888@gmail.com>
2022-08-13 15:20:58 +0800

    Feature weather api

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://weather/./src/index.js?");
+eval("async function getGeocode(city){\r\n    let result = { lat:0 , lon:0};\r\n    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=984837e4bb065d95d91594622e29ca3f`,{mode:\"cors\"});\r\n    const getGeocode = await response.json();\r\n    result.lat = await getGeocode[0].lat;\r\n    result.lon = await getGeocode[0].lon;\r\n    return result;\r\n}\r\nasync function getWeather(geo={lat:0,lon:0}){\r\n    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo.lat}&lon=${geo.lon}&appid=984837e4bb065d95d91594622e29ca3f`,{mode:\"cors\"});\r\n    const weather = await response.json();\r\n    console.log(weather.name);\r\n    return weather.weather[0].main;\r\n}\r\nasync function whatWeather(){\r\n    let city = prompt(\"enter city\");\r\n    const result = await getGeocode(city);\r\n    const weather = await getWeather(result);\r\n    console.log(weather);\r\n}\r\n\r\nwhatWeather();\n\n//# sourceURL=webpack://weather/./src/index.js?");
 
 /***/ })
 
