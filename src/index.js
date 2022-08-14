e3165ac90867565af31b75c4fddebe01f2f459b8
L3OL1N <z3650888@gmail.com>
2022-08-14 12:00:21 +0800

    Feature gif show

--- src/index.js
+++ src/index.js
@@ -18,15 +18,24 @@ async function getWeather(geo={lat:0,lon:0}){
     console.log(weather.name);
     return weather.weather[0].main;
 }
-async function whatWeather(){
+async function getGif(weather){
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=tuKdNPTJOA9Dwp8OTawwzHYS8iMH8mBa&s=${weather}`);
+    const gifData = await response.json();
+    return gifData.data.images.original.url;
+}
+async function sumbitHandle(){
     const city = document.querySelector("form").elements.city.value;
     document.querySelector("h2").innerHTML =  city;
     const result = await getGeocode(city);
     const weather = await getWeather(result);
     document.querySelector("h3").innerHTML = weather;
     console.log(weather);
+    const img = await getGif(weather);
+    document.querySelector("img").src = img;
+    document.querySelector("img").style.display = "block";
+
 }
 
     const sumitBtn = document.querySelector("[type=button]");
-    sumitBtn.addEventListener("click",whatWeather);
+    sumitBtn.addEventListener("click",sumbitHandle);
 
