1b116f9963326f5420652f4e1707015b60ffb3cd
L3OL1N <z3650888@gmail.com>
2022-08-14 11:28:13 +0800

    Feature html show

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 async function getGeocode(city){
-    let result = { lat:0 , lon:0};
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=984837e4bb065d95d91594622e29ca3f`,{mode:"cors"});
-    const getGeocode = await response.json();
-    result.lat = await getGeocode[0].lat;
-    result.lon = await getGeocode[0].lon;
-    return result;
+    try{
+        let result = { lat:0 , lon:0 };   
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=984837e4bb065d95d91594622e29ca3f`,{mode:"cors"});
+        const getGeocode = await response.json();
+        result.lat = await getGeocode[0].lat;
+        result.lon = await getGeocode[0].lon;
+        return result;
+    }
+    catch(e){
+        console.log(e);
+    }
+ 
 }
 async function getWeather(geo={lat:0,lon:0}){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo.lat}&lon=${geo.lon}&appid=984837e4bb065d95d91594622e29ca3f`,{mode:"cors"});
@@ -13,10 +19,14 @@ async function getWeather(geo={lat:0,lon:0}){
     return weather.weather[0].main;
 }
 async function whatWeather(){
-    let city = prompt("enter city");
+    const city = document.querySelector("form").elements.city.value;
+    document.querySelector("h2").innerHTML =  city;
     const result = await getGeocode(city);
     const weather = await getWeather(result);
+    document.querySelector("h3").innerHTML = weather;
     console.log(weather);
 }
 
-whatWeather();
+    const sumitBtn = document.querySelector("[type=button]");
+    sumitBtn.addEventListener("click",whatWeather);
+
